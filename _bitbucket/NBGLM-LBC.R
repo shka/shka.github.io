8d3d36cf91341633cd31f6859a6b3a77327b974b
Shintaro Katayama <shintaro.katayama@gmail.com>
2019-05-28 09:06:20 +0200

    Modify URL to specify the latest source code.

--- _bitbucket/NBGLM-LBC.R
+++ _bitbucket/NBGLM-LBC.R
@@ -1,1 +1,1 @@
-source("https://bitbucket.org/!api/2.0/snippets/shka/GexekX/0b89ec46c5570e07bf93bbcde9eb2ed5a9ad07a6/files/library_bias_correction.R")
+source("https://bitbucket.org/!api/2.0/snippets/shka/GexekX/files/library_bias_correction.R")
